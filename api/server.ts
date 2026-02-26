8eb45de31c1e50dafb4707f7e59634c24ba3b045
Katia Santos <katia.lage@gmail.com>
2026-02-25 21:12:14 -0700

    Refactor server initialization and cleanup
    Removed commented-out code and simplified server start logic.

--- api/server.ts
+++ api/server.ts
@@ -2,8 +2,6 @@ import express, { Application, Request, Response } from 'express';
 import crahelperRouter from './routes/crahelper';
 import serverless from 'serverless-http';
 
-// const express = require('express');
-// const app = express();
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
@@ -14,11 +12,10 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use('/crahelper', crahelperRouter);
 
-if (process.env.NODE_ENV === 'development') {
-  app.listen(PORT, () => {
+
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-  }); 
-}
+}); 
 
 // Export for Vercel
 export default serverless(app);
