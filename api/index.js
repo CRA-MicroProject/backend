250964f5451fdd83ecdd99df0ee578bcf6ab5ace
Youssef Ghaly <youssefadly47@yahoo.com>
2026-03-19 16:42:35 -0600

    build error handled

--- api/index.js
+++ api/index.js
@@ -1,2 +1,2 @@
 // Vercel serverless entry: all requests are handled by the Express app (TS default export → .default)
-module.exports = require('../server').default;
+module.exports = require('../dist/server').default;
